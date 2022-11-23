8c8a7153d2732646ffa0366b03100db1d54e2bcc
Olga Panayot <panayot.olga@gmail.com>
2022-11-23 18:08:02 +0200

    do 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+	gallery: document.querySelector(".gallery"),
+};
+const galleryList = makeGalleryItems(galleryItems);
+refs.gallery.insertAdjacentHTML("beforeend", galleryList);
+
+function makeGalleryItems(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `<a class="gallery__item" href="${original}">
+			<img class="gallery__image" src="${preview}" alt="${description}" />
+			</a>`;
+		})
+		.join("");
+}
+const lightbox = new SimpleLightbox(".gallery__item", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
